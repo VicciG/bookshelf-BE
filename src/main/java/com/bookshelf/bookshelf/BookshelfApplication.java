c0aa16439f94356ce91f133c3c5da47839ff2777
VicciG <vicci.garner@hotmail.co.uk>
2021-01-29 13:21:51 +0000

    DB connection

--- src/main/java/com/bookshelf/bookshelf/BookshelfApplication.java
+++ src/main/java/com/bookshelf/bookshelf/BookshelfApplication.java
@@ -1,16 +1,34 @@
 package com.bookshelf.bookshelf;
 
+import org.jdbi.v3.core.Handle;
+import org.jdbi.v3.core.Jdbi;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.util.List;
 
 @SpringBootApplication
 public class BookshelfApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(BookshelfApplication.class, args);
-        BookshelfController ctrl = new BookshelfController();
-        ctrl.sayHello("Vicci");
+        try {
+            Jdbi jdbi = Jdbi.create("jdbc:postgresql://bookshelfdb.cbktbt3i6mfv.eu-west-2.rds.amazonaws.com:5433/postgres?user=postgres&password=kYIAgWW5VZYT54Et9zDo");
+            Handle h = jdbi.open();
+            //h.execute("CREATE TABLE book (bookId INTEGER PRIMARY KEY, title VARCHAR, author VARCHAR, colour VARCHAR)");
+            h.execute("INSERT INTO book VALUES (?, ?, ?, ?)", 4, "Roald Dahl", "The Twits", "Green");
 
+           /* List<Book> list = h.createQuery("SELECT title, author FROM book").map(new BookMapper()).list();
+
+            for (Book a : list) {
+                System.out.println("Title: " + a.getTitle() + " Author: " + a.getAuthor());
+            }*/
+
+            h.close();
+        }
+        catch (Exception e){
+            e.printStackTrace();
+        }
     }
 
 }
--- src/main/java/com/bookshelf/bookshelf/BookshelfController.java
+++ src/main/java/com/bookshelf/bookshelf/BookshelfController.java
@@ -1,7 +1,20 @@
 package com.bookshelf.bookshelf;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class BookshelfController {
-    public void sayHello(String name){
-        System.out.println("Hello " + name);
+    @GetMapping("/name/{name}")
+    public String sayHello(@PathVariable String name){
+        return "Hello " + name;
     }
 }
+
+// TO DO
+// Set up angular project
+//set up api endpoints here
+//get db working, alternatively setup localdb
